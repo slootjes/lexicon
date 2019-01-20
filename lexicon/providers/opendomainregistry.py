09f030381caaebe9a8f884577f792b461ed3d357
Robert <robertslootjes@gmail.com>
2019-01-20 13:25:09 +0100

    Improve current code

--- lexicon/providers/opendomainregistry.py
+++ lexicon/providers/opendomainregistry.py
@@ -47,41 +47,24 @@ class Provider(BaseProvider):
 
         self.access_token = token_response['response']['token']
 
-    # List all records. Return an empty list if no records found
-    # type, name and content are used to filter records.
-    # If possible filter during the query, otherwise filter after response is received.
     def _list_records(self, rtype=None, name=None, content=None):
+        return self._get_current_records(None, rtype, name, content)
 
-        domain_id = self._get_domain_identifier()
-        records_response = self._get('/dns/{0}'.format(domain_id))
+    def _create_record(self, rtype, name, content):
+        if self._get_domain_identifier() is None:
+            # domain does not yet exist, create new zone for it
+            return True
+        else:
+            # domain zone exists, add current record to it
+            return self._update_record(None, rtype=rtype, name=name, content=content)
 
-        entries = []
+    def _update_record(self, identifier, rtype=None, name=None, content=None):
 
-        for record in records_response['response']['records']:
 
-            LOGGER.debug("Record: %s", record)
+        return True
 
-            if (rtype is not None) and (record['type'] != rtype):
-                LOGGER.debug(
-                    "\tType doesn't match - expected: '%s', found: '%s'",
-                    rtype, record['type'])
-                continue
-
-            entry = {
-                'type': record['type'],
-                'name': record['name'],
-                'ttl': record['ttl'],
-                'content': record['content'],
-                'options': {
-                    'disabled': record['disabled']
-                }
-            }
-
-            entries.append(entry)
-
-        LOGGER.debug('list_records: %s', entries)
-
-        return entries
+    def delete_record(self, identifier=None, rtype=None, name=None, content=None, **kwargs):
+        return True
 
     def _request(self, action='GET', url='/', data=None, query_params=None):
         if data is None:
@@ -105,6 +88,47 @@ class Provider(BaseProvider):
         return response.json()
 
     # Helpers
+    def _get_current_records(self, identifier=None, rtype=None, name=None, content=None):
+        domain_id = self._get_domain_identifier()
+        records_response = self._get('/dns/{0}'.format(domain_id))
+
+        matches = []
+        for record in records_response['response']['records']:
+
+            LOGGER.debug('Record: %s', record)
+
+            if (rtype is not None) and (record['type'] != rtype):
+                LOGGER.debug('Type does not match - expected "%s", found "%s"', rtype, record['type'])
+                continue
+
+            if (name is not None) and (record['name'] != name):
+                LOGGER.debug('Name does not match - expected "%s", found "%s"', name, record['name'])
+                continue
+
+            if (name is not None) and (record['content'] != content):
+                LOGGER.debug('Content does not match - expected "%s", found "%s"', content, record['content'])
+                continue
+
+            record_id = self._create_record_identifier(record)
+            if (identifier is not None) and (identifier != record_id):
+                LOGGER.debug('Identifier does not match - expected "%s", found "%s"', identifier, record_id)
+                continue
+
+            matches.append({
+                'id': record_id,
+                'type': record['type'],
+                'name': record['name'],
+                'ttl': record['ttl'],
+                'content': record['content'],
+                'options': {
+                    'disabled': record['disabled']
+                }
+            })
+
+        LOGGER.debug('list_records: %s', matches)
+
+        return matches
+
     def _get_domain_identifier(self):
         if self.domain_id is not None:
             return self.domain_id
@@ -123,3 +147,11 @@ class Provider(BaseProvider):
 
     def _calculate_hash(self, value):
         return hashlib.sha1(value.encode('utf-8')).hexdigest()
+
+    def _create_record_identifier(self, record):
+        record_hash = hashlib.sha1()
+        record_hash.update(('type=' + record['type'] + ',').encode('utf-8'))
+        record_hash.update(('name=' + record['name'] + ',').encode('utf-8'))
+        record_hash.update(('content=' + record['content'] + ',').encode('utf-8'))
+
+        return record_hash.hexdigest()
